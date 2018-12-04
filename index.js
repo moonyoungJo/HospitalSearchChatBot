46c8d70cb89ab55e1cdc5cffb977001353794a77
moonyoungJo <jomy9649@gmail.com>
2018-12-04 17:41:23 +0900

    Use egan api

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const bodyParser = require('body-parser');
 const rp = require('request-promise');
 
 require('dotenv').config();
-const KAKAO_KEY = process.env.KAKAO_KEY;
-const PORT = process.env.PORT;
-const SERVER_URI = `http://${process.env.SERVER_ADDRESS}:${PORT}/static/`;
+const PORT = process.env.PORT || 80;
+const SERVER_ADDRESS = process.env.SERVER_ADDRESS || '13.209.83.224';
+const SERVER_URI = `http://${SERVER_ADDRESS}:${PORT}/static/`;
+const egenAPI = 'https://0lpqwmpv11.execute-api.ap-northeast-2.amazonaws.com/default/';
 const app = express();
-const KEYWORD_SEARCH_URI = 'https://dapi.kakao.com/v2/local/search/keyword.json';
 
 app.use('/static', express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-const server = app.listen(PORT || 80, 
+const server = app.listen(PORT, 
   () => {
     console.log('hospital search server is listening');
 });
@@ -44,51 +44,47 @@ app.post('/api/searchHospital', async function (req, res) {
     return;
   }
   try{
-    let page = 1;
-    let nextLoop = true;
-
     //search open hospital
-    while(nextLoop){
-      let search_uri = KEYWORD_SEARCH_URI + 
-        encodeURI('?query='+ department) + 
-        '&x='+x+
-        '&y='+y+
-        '&category_group_code=HP8&radius=10000&sort=distance&page=' + page;
-      page++;
-
-      let response = await rp({
-        uri: search_uri, 
-        method: "GET", 
-        headers: {
-          'Authorization': KAKAO_KEY
-        }
-      });
-      response = JSON.parse(response);
-
-      if(response.meta.is_end){
-        nextLoop = false;
-      }
-      let totalCount = response.meta.total_count;
-      for(let index = 0; index < totalCount; index++){
-        let document = response.documents[index];
-        //크롤러로 모은 정보를 이용해 열려있는지 확인하기
-        let description = `앙몬드봇이 가장 가까운 ${department}를 찾았습니다.` +  
-          `\n\n병원명: ${document.place_name}\n위치 : ${document.address_name}`;
-        if(document.phone !== undefined){
-          description += `\n전화번호 : ${document.phone}`;
-        }
-        res.send(makeResponse(
-          document.place_name,
-          description,
-          SERVER_URI+'search_hospital.jpg',
-          document.place_url
-        ));
-        return;   
-      }
+    let search_uri = egenAPI + 
+      'searchHospital?lon='+x+
+      '&lat='+y+
+      '&available=1';
+    if(department !== '병원'){
+      search_uri = search_uri + '&dept=' + encodeURI(department);
     }
-    res.send(makeTextResponse(`근처에 열려있는 ${department}가 없습니다... `));
+
+    let response = await rp({
+      uri: search_uri, 
+      method: "GET", 
+    });
+    response = JSON.parse(response);
+
+    //process response
+    if(!response.success){
+      res.send(makeTextResponse(`response of egen server is error... `));
+      return;
+    }
+    if(response.result.length === 0){
+      res.send(makeTextResponse(`근처에 열려있는 ${department}가 없습니다... `));
+      return;
+    }
+    const document = response.result[0];
+    let description = `앙몬드봇이 가장 가까운 ${department}를 찾았습니다.` +  
+        `\n\n병원명: ${document.name}` + 
+        `\n위치 : ${document.address}` +
+        `\n분류 : ${document.category}` +
+        `\n전화번호 : ${document.contact}`;
+
+    res.send(makeResponse(
+      document.name,
+      description,
+      SERVER_URI+'search_hospital.jpg',
+      document.url_kakaomap
+    ));
+    return;   
+    
   }catch(err){
-    res.send(makeTextResponse('지도 api 오류'));
+    res.send(makeTextResponse('egan api 오류'));
   }
 })
 
@@ -109,53 +105,44 @@ app.post('/api/searchPharmacy', async function (req, res) {
     return;
   }
 
-  //search with kakao map api
   try{
-    let page = 1;
-    let nextLoop = true;
+    //search open pharmacy
+    let search_uri = egenAPI + 
+      'searchPharmacy?lon='+x+
+      '&lat='+y+
+      '&available=1';
 
-    //search open hospital
-    while(nextLoop){
-      let search_uri = KEYWORD_SEARCH_URI + 
-        encodeURI('?query=약국') + 
-        '&x='+x+
-        '&y='+y+
-        '&category_group_code=PM9&radius=10000&sort=distance&page=' + page;
-      page++;
+    let response = await rp({
+      uri: search_uri, 
+      method: "GET", 
+    });
+    response = JSON.parse(response);
 
-      let response = await rp({
-        uri: search_uri, 
-        method: "GET", 
-        headers: {
-          'Authorization': KAKAO_KEY
-        }
-      });
-      response = JSON.parse(response);
-
-      if(response.meta.is_end){
-        nextLoop = false;
-      }
-      let totalCount = response.meta.total_count;
-      for(let index = 0; index < totalCount; index++){
-        let document = response.documents[index];
-        //크롤러로 모은 정보를 이용해 열려있는지 확인하기
-        let description = `앙몬드 봇이 가장 가까운 약국을 찾았습니다. ` + 
-          `\n\n약국명: ${document.place_name}\n위치 : ${document.address_name}`;
-        if(document.phone !== undefined){
-          description += `\n전화번호 : ${document.phone}`;
-        }
-        res.send(makeResponse(
-          document.place_name,
-          description,
-          SERVER_URI+'search_pharmacy.jpg',
-          document.place_url
-        ));
-        return;   
-      }
+    //process response
+    if(!response.success){
+      res.send(makeTextResponse(`response of egen server is error... `));
+      return;
     }
-    res.send(makeTextResponse(`근처에 열려있는 약국이 없습니다... `));
+    if(response.result.length === 0){
+      res.send(makeTextResponse(`근처에 열려있는 약국이 없습니다... `));
+      return;
+    }
+    const document = response.result[0];
+    let description = `앙몬드봇이 가장 가까운 약국을 찾았습니다.` +  
+        `\n\n약국명: ${document.name}` + 
+        `\n위치 : ${document.address}` +
+        `\n전화번호 : ${document.contact}`;
+
+    res.send(makeResponse(
+      document.name,
+      description,
+      SERVER_URI+'search_pharmacy.jpg',
+      document.url_kakaomap
+    ));
+    return;   
+    
   }catch(err){
-    res.send(makeTextResponse('지도 api 오류'));
+    res.send(makeTextResponse('egan api 오류'));
   }
 })
 
@@ -176,58 +163,45 @@ app.post('/api/searchEmergencyRoom', async function (req, res) {
     return;
   }
 
-  //search with kakao map api
   try{
-    let page = 1;
-    let nextLoop = true;
+    //search open emergency room
+    let search_uri = egenAPI + 
+      'searchHospital?lon='+x+
+      '&lat='+y+
+      '&available=1&emergency=1';
 
-    //search open hospital
-    while(nextLoop){
-      let search_uri = KEYWORD_SEARCH_URI + 
-        encodeURI('?query=응급실') + 
-        '&x='+x+
-        '&y='+y+
-        '&radius=10000&sort=distance&page=' + page;
-      page++;
+    let response = await rp({
+      uri: search_uri, 
+      method: "GET", 
+    });
+    response = JSON.parse(response);
 
-      let response = await rp({
-        uri: search_uri, 
-        method: "GET", 
-        headers: {
-          'Authorization': KAKAO_KEY
-        }
-      });
-      response = JSON.parse(response);
-
-      if(response.meta.is_end){
-        nextLoop = false;
-      }
-      let totalCount = response.meta.total_count;
-      console.log(response.documents);
-      for(let index = 0; index < totalCount; index++){
-        let document = response.documents[index];
-        if(document.category_name !== '의료,건강 > 병원 > 응급실'){
-          continue;
-        }
-        //크롤러로 모은 정보를 이용해 열려있는지 확인하기
-        let description = `\n앙몬드 봇이 가장 가까운 응급실을 찾았습니다. \n\n병원명: ${document.place_name}` + 
-          `\n위치 : ${document.address_name}`;
-        if(document.phone !== undefined){
-          description += `\n전화번호 : ${document.phone}`;
-        }
-        console.log(SERVER_URI+'search_emergency.jpg');
-        res.send(makeResponse(
-          document.place_name,
-          description,
-          SERVER_URI+'search_emergency.jpg',
-          document.place_url
-        ));
-        return;   
-      }
+    //process response
+    if(!response.success){
+      res.send(makeTextResponse(`response of egen server is error... `));
+      return;
     }
-    res.send(makeTextResponse(`근처에 응급실이 없습니다... `));
+    if(response.result.length === 0){
+      res.send(makeTextResponse(`근처에 열려있는 응급실이 없습니다... `));
+      return;
+    }
+    const document = response.result[0];
+    let description = `앙몬드봇이 가장 가까운 응급실을 찾았습니다.` +  
+        `\n\n병원명: ${document.name}` + 
+        `\n위치 : ${document.address}` +
+        `\n분류 : ${document.category}` +
+        `\n전화번호 : ${document.contact}`;
+
+    res.send(makeResponse(
+      document.name,
+      description,
+      SERVER_URI+'search_emergency.jpg',
+      document.url_kakaomap
+    ));
+    return;   
+    
   }catch(err){
-    res.send(makeTextResponse('지도 api 오류'));
+    res.send(makeTextResponse('egan api 오류'));
   }
 })
 
@@ -265,27 +239,5 @@ function makeResponse(title, description, imageURL, buttonURL){
         }
       ]
     }
-  }
-}
-function makeButtonResponse(description, buttonDescript, buttonURL){
-  return {"contents":[
-      {
-        "type":"card.text",
-        "cards":[
-          {
-            "description":description,
-            "buttons":[
-              {
-                "type":"url",
-                "label":buttonDescript,
-                "data":{
-                  'url':buttonURL
-                }
-              }
-            ]
-          }
-        ]
-      }
-    ]
   }
 }
